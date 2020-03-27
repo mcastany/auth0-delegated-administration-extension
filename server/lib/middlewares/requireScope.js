e221084b635b967378f6341ef11734bc070dc119
Marcos Castany <marcos.castany@auth0.com>
2020-03-26 23:00:19 -0300

    More changes

--- server/lib/middlewares/requireScope.js
+++ server/lib/middlewares/requireScope.js
@@ -1,7 +1,7 @@
 import { ForbiddenError } from 'auth0-extension-tools';
 
 export default (expectedScope) => (req, res, next) => {
-  console.log('marcos', req.user.scope, expectedScope)
+  console.log('marcos', req.user && req.user.scope, expectedScope)
   if (!req.user || !req.user.scope || req.user.scope.indexOf(expectedScope) < 0) {
     return next(new ForbiddenError(`Cannot perform action. Missing scope ${expectedScope}`));
   }
