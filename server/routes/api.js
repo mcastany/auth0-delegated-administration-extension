8ce4169fe33d609816b345724f2cfd4c21168910
Marcos Castany <marcos.castany@auth0.com>
2020-04-01 21:52:11 -0300

    More changes

--- server/routes/api.js
+++ server/routes/api.js
@@ -68,7 +68,7 @@ export default (storage) => {
 
   // Allow end users to authenticate.
   api.use(middlewares.authenticateUsers.optional({
-    domain: config('AUTH0_CUSTOM_DOMAIN') || config('AUTH0_DOMAIN'),
+    domain:  config('AUTH0_CUSTOM_DOMAIN') || config('AUTH0_DOMAIN'),
     audience: config('EXTENSION_CLIENT_ID'),
     credentialsRequired: false,
     onLoginSuccess: (req, res, next) => {
@@ -116,13 +116,7 @@ export default (storage) => {
   api.use('/applications', managementApiClient, applications());
   api.use('/connections', managementApiClient, connections(scriptManager));
   api.use('/scripts', requireScope(constants.ADMIN_PERMISSION), scripts(storage, scriptManager));
-  api.use('/users', (req, res, next) => {
-    tools.managementApi.getAccessToken(config('AUTH0_DOMAIN'), config('AUTH0_CLIENT_ID'), config('AUTH0_CLIENT_SECRET'))
-      .then(function(token) {
-        console.log('marcos', token)
-        next();
-      })
-  }, managementApiClient, users(storage, scriptManager));
+  api.use('/users', managementApiClient, users(storage, scriptManager));
   api.use('/logs', managementApiClient, logs(scriptManager));
   api.use('/me', me(scriptManager));
   api.get('/settings', (req, res, next) => {
